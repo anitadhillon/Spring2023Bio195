32353a5747c0fb3cd90b724ae9de7b27e3579270
Anita Dhillon <124739712+anitadhillon@users.noreply.github.com>
2023-02-24 11:47:41 -0800

    codes

---
+++ Data-raw/2023-7-2versioncontrol.R
@@ -0,0 +1,33 @@
+# Intro to version control
+
+## why version control systems?
+## efficient way of keeping tack of changes in a document 
+## keep track of changes in any type of document : txt, table, images
+
+## conflicts in version control 
+# conflict happens when someone changes the same part of a document 
+# at the same time 
+- 
+## What is a commit
+- saves the changes made to files to a repository 
+- it does not have to be all files at a time 
+
+## what is a repository 
+- it is the whole of files and file versions, along with the 
+history of changes made (commits)
+
+# Difference between old and modern version control systems
+- centralization 
+
+# setting up git
+
+## creating branches 
+
+
+
+
+
+
+
+
+
